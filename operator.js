e4cba984edbc0bc8a1dc9b54db4ea1394b85c68b
Tharak-2005 <tharakbf@gmail.com>
2025-09-06 14:59:13 +0530

    operators completed

--- operator.js
+++ operator.js
@@ -1,42 +1,42 @@
-// //Arithmetic operations
+//Arithmetic operations
 
-// let x=10,y=10
+let x=10,y=10
 
-// console.log(x+y)
-// console.log(x*y)
-// console.log(x%y)
-// console.log(x-y)
+console.log(x+y)
+console.log(x*y)
+console.log(x%y)
+console.log(x-y)
 
-// let z=5
-// z+=3
-// console.log(z)
-// z*=2
-// console.log(z)
-// z-=4
-// console.log(z)
-// z%=3
-// console.log(z)
+let z=5
+z+=3
+console.log(z)
+z*=2
+console.log(z)
+z-=4
+console.log(z)
+z%=3
+console.log(z)
 
-// //comparision 
+//comparision 
 
-// let a=18,b="18"
+let a=18,b="18"
 
-// console.log(a==b)
-// console.log(a+b)
-// console.log(a!=b)
-// console.log(a===b)
-// console.log(a!==b)
-// console.log(a>b)
-// console.log(a<b)
-// console.log(a>=b)
-// console.log(a<=b)
+console.log(a==b)
+console.log(a+b)
+console.log(a!=b)
+console.log(a===b)
+console.log(a!==b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
 
-// //logical operators
+//logical operators
 
-// let age=20
-// console.log(age>=20 && age<=40)
-// console.log(age<20 || age>20)
-// console.log(!(age<18))
+let age=20
+console.log(age>=20 && age<=40)
+console.log(age<20 || age>20)
+console.log(!(age<18))
 //string operators
 
 let firstname="Tharaka"
@@ -57,3 +57,15 @@ else{
 
 let result=myAge>=18 ? "elibile for vote":"not eligible"
 console.log(result)
+
+//increment decrement
+
+let count=5
+console.log(count++)
+console.log(count)
+console.log(++count)
+
+//typeof operators
+console.log(typeof count)
+console.log(typeof "Tharak")
+console.log(typeof 18)
