55bd195c2b55c3efee17b008fe5876413c127150
Tharak-2005 <tharakbf@gmail.com>
2025-09-06 12:34:55 +0530

    js

--- practice.js
+++ practice.js
@@ -1,1 +1,33 @@
 console.log("Hello World, OnePiece")
+
+let name="Tharaka Ram"
+console.log(name)
+
+let num=18
+console.log(num)
+
+let islogged=false
+console.log(islogged)
+
+let x
+console.log(x)
+let y=null
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+console.log(typeof x)
+console.log(typeof y)
+
+let arr=[1,2,3,4,5]
+console.log(arr[4])
+console.log(typeof arr)
+
+let person={
+    name:"Tharak",
+    age:19
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
